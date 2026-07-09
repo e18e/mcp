6c83e8b4a0314284b7e4c272a58262c1ff28ffd2
paoloricciuti <ricciutipaolo@gmail.com>
2026-07-09 12:50:47 +0200

    fix: use proper type

--- packages/stdio/src/tools/utils.ts
+++ packages/stdio/src/tools/utils.ts
@@ -1,21 +1,17 @@
 import microUtilsReplacements from 'module-replacements/manifests/micro-utilities.json' with { type: 'json' };
 import preferredReplacements from 'module-replacements/manifests/preferred.json' with { type: 'json' };
+import type { ModuleReplacement } from 'module-replacements';
 import { get_docs } from '../docs/index.js';
-
-type ModuleReplacement =
-	| (typeof microUtilsReplacements.moduleReplacements)[number]
-	| (typeof preferredReplacements.moduleReplacements)[number];
 
 const docs = await get_docs();
 
 export function get_suggestions_for_package(pkg: string) {
-	let replacement: ModuleReplacement | undefined =
-		microUtilsReplacements.moduleReplacements.find(
-		(replacement) => replacement.moduleName === pkg,
-	);
+	let replacement: ModuleReplacement | undefined = (
+		microUtilsReplacements.moduleReplacements as ModuleReplacement[]
+	).find((replacement) => replacement.moduleName === pkg);
 	replacement =
 		replacement ??
-		preferredReplacements.moduleReplacements.find(
+		(preferredReplacements.moduleReplacements as ModuleReplacement[]).find(
 			(replacement) => replacement.moduleName === pkg,
 		);
 	if (
@@ -23,13 +19,12 @@ export function get_suggestions_for_package(pkg: string) {
 		replacement.type !== 'native' &&
 		replacement.type !== 'none'
 	) {
-		if ('docPath' in replacement) {
+		if (replacement.type === 'documented') {
 			if (!docs[replacement.docPath + '.md']) return;
 			return `Don't use \`${pkg}\` instead read the following document:\n\n${
 				docs[replacement.docPath + '.md']
 			}`;
 		}
-		if (!('replacement' in replacement)) return;
 		return `Don't use \`${pkg}\` instead ${replacement.replacement}`;
 	}
 	return;
